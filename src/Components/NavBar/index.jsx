4d4539ce5e20c0c44a8bd7b3e1bb52932b30f98b
oscar.franco <ofrubio0@gmail.com>
2023-06-03 14:06:16 -0500

    Se finaliza el componente NavBar

---
+++ src/Components/NavBar/index.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom"
+
+const NavBar = () =>{
+    const activeStyles = 'underline underline-offset-4'
+    return(
+        <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
+            <ul className='flex items-center gap-3'>
+                <li className='font-semibold text-lg'><NavLink to='/'>Shopi</NavLink></li>
+                <li><NavLink to='/' className={({isActive})=> isActive ? activeStyles : undefined}>All</NavLink></li>
+                <li><NavLink to='/clotes' className={({isActive})=> isActive ? activeStyles : undefined}>Clotes</NavLink></li>
+                <li><NavLink to='/electronic' className={({isActive})=> isActive ? activeStyles : undefined}>Electronic</NavLink></li>
+                <li><NavLink to='/furnitures' className={({isActive})=> isActive ? activeStyles : undefined}>Furnitures</NavLink></li>
+                <li><NavLink to='/toys' className={({isActive})=> isActive ? activeStyles : undefined}>Toys</NavLink></li>
+                <li><NavLink to='/others' className={({isActive})=> isActive ? activeStyles : undefined}>Others</NavLink></li>
+            </ul>
+            <ul className='flex items-center gap-3'>
+                <li className='text-black/60'>rubio@rubiocorp.com</li>
+                <li><NavLink to='/my-orders' className={({isActive})=> isActive ? activeStyles : undefined}>My Orders</NavLink></li>
+                <li><NavLink to='/my-account' className={({isActive})=> isActive ? activeStyles : undefined}>My Account</NavLink></li>
+                <li><NavLink to='/sign-in' className={({isActive})=> isActive ? activeStyles : undefined}>Sign-In</NavLink></li>
+                <li>🛒0</li>
+                
+            </ul>
+        </nav>
+    )
+}
+export { NavBar }
