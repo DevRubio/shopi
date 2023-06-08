1f2576183932b61fb6da44c67ce51affb811042f
oscar.franco <ofrubio0@gmail.com>
2023-06-07 19:08:53 -0500

    Se crea Context para usar estados globales en la Aplicacion

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,5 @@
 import { useRoutes, BrowserRouter } from "react-router-dom";
+import { ContextProvider } from "../../Context";
 import { Home } from "../Home";
 import { MyAccount } from "../MyAccount";
 import { MyOrder } from "../MyOrder";
@@ -22,10 +23,13 @@ const AppRoutes = () => {
 
 const App = () => {
   return (
+    <ContextProvider>
     <BrowserRouter>
       <AppRoutes />
       <NavBar/>
     </BrowserRouter>
+    </ContextProvider>
+
   );
 };
 
